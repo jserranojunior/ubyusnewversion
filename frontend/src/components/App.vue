4673396ebf1b30fce014e2e2f12dc1a2ae40e299
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2023-03-25 16:23:38 -0300

    Template

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,15 +1,8 @@
 <script setup lang="ts">
-  import ProdutosVue from "./ProdutosModal.vue";
   import { onMounted } from "vue";
   import Navbar from "./Navbar.vue";
   import Footer from "./Footer.vue";
   import Home from "./Home.vue";
-  /*   import Infraestrutura from "./Infraestrutura.vue";
-  import Produtos from "./Produtos.vue";
-  import Clientes from "./Clientes.vue";
-  import Certificados from "./Certificados.vue";
-  import Premios from "./Premios.vue";
-  import Contatos from "./Contatos.vue";*/
   import BtnToTop from "./BtnToTop.vue";
 
   onMounted(() => {});
@@ -20,7 +13,7 @@
     <div class="bg-base-100">
       <div class="content px-0 md:px-0 lg:px-0 2xl:px-24">
         <Navbar></Navbar>
-        <router-view />
+        <router-view></router-view>
         <Footer></Footer>
         <BtnToTop></BtnToTop>
       </div>
